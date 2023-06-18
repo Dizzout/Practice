d6a26f06b18bf63227f9c291431911db4c0e8d4e
Dizzout <136970918+Dizzout@users.noreply.github.com>
2023-06-18 15:16:16 +0300

    Update code.java

--- idz1/task4/code.java
+++ idz1/task4/code.java
@@ -1,1 +1,26 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int data[] = {3, 6, 7, 10, 34, 56, 60};
+        int numberToFind = 10;
+
+        int low = 0;
+        int high = data.length - 1;
+        int position = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (data[mid] == numberToFind) {
+                position = mid;
+                break;
+            } else if (data[mid] < numberToFind) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        System.out.println(position);
+    }
+}
 
