a1bb80c335ad2378ec844862644d6b4b5fa3d97f
Dizzout <136970918+Dizzout@users.noreply.github.com>
2023-06-18 14:49:33 +0300

    Update code.java

--- idz1/task1/code.java
+++ idz1/task1/code.java
@@ -1,1 +1,36 @@
+public class SquareRoot {
+
+    public static void main(String[] args) {
+        double a = 3;
+        double b = 2.5;
+        double c = -0.5;
+
+        if (a == 0) {
+            if (b == 0) {
+                System.out.println("x1=");
+                System.out.println("x2=");
+            } else {
+                double x = -c / b;
+                System.out.println("x1=" + Math.abs(x));
+                System.out.println("x2=" + Math.abs(x));
+            }
+        } else {
+            double discriminant = b * b - 4 * a * c;
+
+            if (discriminant > 0) {
+                double x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+                double x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+                System.out.println("x1=" + x1);
+                System.out.println("x2=" + x2);
+            } else if (discriminant == 0) {
+                double x = -b / (2 * a);
+                System.out.println("x1=" + x);
+                System.out.println("x2=" + x);
+            } else {
+                System.out.println("x1=");
+                System.out.println("x2=");
+            }
+        }
+    }
+}
 
