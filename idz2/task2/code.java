781c44cc43ecee4bde65239b0b59059ae7234381
Dizzout <136970918+Dizzout@users.noreply.github.com>
2023-06-18 15:22:19 +0300

    Update code.java

--- idz2/task2/code.java
+++ idz2/task2/code.java
@@ -1,1 +1,14 @@
+package com.tasks3.fibonacci;
+
+public class Fibonacci {
+    public long getNumber(int position) {
+        if (position <= 0) {
+            return -1; // якщо позиція виходить за межі допустимого діапазону
+        } else if (position == 1 || position == 2) {
+            return 1; // базовий випадок, коли позиція дорівнює 1 або 2
+        } else {
+            return getNumber(position - 1) + getNumber(position - 2); // рекурсивний виклик
+        }
+    }
+}
 
