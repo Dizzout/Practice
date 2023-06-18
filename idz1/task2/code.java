f6d19fab09dbe9ea4d3b2216cd7b7642f2ec1d29
Dizzout <136970918+Dizzout@users.noreply.github.com>
2023-06-18 15:14:04 +0300

    Update code.java

--- idz1/task2/code.java
+++ idz1/task2/code.java
@@ -1,1 +1,18 @@
+public class MatrixPrint {
+    public static void main(String[] args) {
+        int size = 5;
+
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (i == j || i == size - j + 1) {
+                    System.out.print(" * ");
+                } else {
+                    int number = (i - 1) * size + j;
+                    System.out.print(String.format("%2d ", number));
+                }
+            }
+            System.out.println();
+        }
+    }
+}
 
